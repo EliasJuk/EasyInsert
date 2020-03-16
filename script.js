cbfb730da5fe464aab9f0dd6d691e79434fcde7d
eliasjuk <eliasp.juk@hotmail.com>
2020-03-16 10:56:00 -0300

    Adicionado Options

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var mysql = require('mysql');
-const faker = require('faker')
+const faker = require('faker/locale/pt_BR')
 
 function criar(){
     let campos = document.getElementById('nCampos').value
@@ -16,15 +16,26 @@ function criar(){
             newInput.id = 'i'+i //ID DOS INPUTS
         //INSERE INPUT SELECT
             var newSelect = document.createElement('select')
+
             newSelect.innerHTML = '<option class="optionNome">Nome</option>'
+            newSelect.innerHTML += '<option class="optionlastName">lastName</option>'
+            newSelect.innerHTML += '<option class="optionfindName">findName</option>'
+            newSelect.innerHTML += '<option class="optionSobrenome">Sobrenome</option>'
             newSelect.innerHTML += '<option class="optionNMulher">Nome Mulher</option>'
             newSelect.innerHTML += '<option class="optionNHomen">Nome Homen</option>'
-            newSelect.innerHTML += '<option class="optionSobrenome">Sobrenome</option>'
+            newSelect.innerHTML += '<option class="optionNascimento">Nascimento</option>'
+            newSelect.innerHTML += '<option class="optionRG">RG</option>'
+            newSelect.innerHTML += '<option class="optionCPF">CPF</option>'
             newSelect.innerHTML += '<option class="optionEmail">E-mail</option>'
             newSelect.innerHTML += '<option class="optionEndereco">Endereco</option>'
             newSelect.innerHTML += '<option class="optionCidade">Cidade</option>'
             newSelect.innerHTML += '<option class="optionPais">Pais</option>'
+            newSelect.innerHTML += '<option class="optionCep">CEP</option>'
             newSelect.innerHTML += '<option class="optionTelefone">Telefone</option>'
+            newSelect.innerHTML += '<option class="optionFuncao">Funcao</option>'
+            newSelect.innerHTML += '<option class="optionJobTitle">JobTitle</option>'
+            newSelect.innerHTML += '<option class="optionjobArea">jobArea</option>'
+            newSelect.innerHTML += '<option class="optionSalario">salario</option>'
             newSelect.id = 's'+i
         //INSERINDO DENTRO DA DIV
             criaCampos.appendChild(newLabel) //INSERE DENTRO DA DIV
@@ -98,29 +109,125 @@ function insertFaker(){
     //var randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
     //var randomCard = faker.helpers.createCard(); // random contact card containing many properties
 
-    var s1 = document.getElementById('s1').value    
-        
-    let opcao = s1
+    var select = 's1' //POSIÇÃO INICIAL DA BUSCA
+    var buscar = document.getElementById(select).value  //CONSULTA INICIAL 
+    var opcao = buscar //ATRIBUI O VALOR DO PRIMEIRO CAMPO A OPCAO
+    var max = document.getElementById('nCampos').value //NUMERO MAXIMO DE REPETICOES
+    var cont = 0
+    let arrayCad = []
 
-    for(let i=0;i<2;i++){
-        if (s1 == 'Nome') {
-            console.log('nome')
-        }else if (opcao == 'Nome Mulher'){
-            console.log('Nome Mulher')
-        }else if (opcao == 'Nome Homen'){
-            console.log('Nome Homen')
+    for(let i=0;i<max;i++){
+        cont++
+        var select = ('s'+cont)
+        console.log(select)
+        var opcao = document.getElementById(select).value
+        console.log(opcao)
+
+        if (opcao == 'Nome') {
+            var randomName = faker.name.firstName();
+            console.log('Nome:'+ randomName)
+            arrayCad.push(randomName)
+
+        }else if (opcao == 'lastName'){
+            var randomLastName = faker.name.lastName();
+            console.log('Nome do Meio: ' + randomLastName)
+            arrayCad.push(randomLastName)
+
+        }else if (opcao == 'findName'){
+            var randomSobrenome = faker.name.findName();
+            console.log('Ultimo Nome' + randomSobrenome)   
+            arrayCad.push(randomSobrenome)           
+    
         }else if (opcao == 'Sobrenome'){
-            console.log('Sobrenome')
+            var randomSobrenome = faker.name.findName();
+            console.log('Sobrenome' + randomSobrenome)   
+            arrayCad.push(randomSobrenome)           
+
+        }else if (opcao == 'Nome Mulher'){
+            var randomNameFemale = faker.name.findName();
+            console.log('nome:' + randomNameFemale)
+            arrayCad.push(randomNameFemale)
+
+        }else if (opcao == 'Nome Homen'){
+            var randomNameMan = faker.name.findName();
+            console.log('Nome Homen:' + randomNameMan)
+            arrayCad.push(randomNameMan)
+
+        }else if (opcao == 'Nascimento'){
+            var Nascimento = faker.date.month();            
+            console.log('Nascimento:' + Nascimento)
+            arrayCad.push(Nascimento)
+
+        }else if (opcao == 'CPF'){
+            var CPF = faker.address.zipCode();
+            console.log('CPF:' + CPF)
+            arrayCad.push(CPF)
+
+        }else if (opcao == 'RG'){
+            var RG = faker.address.zipCode();
+            console.log('RG:' + RG) 
+            arrayCad.push(RG)   
+    
         }else if (opcao == 'E-mail'){
-            console.log('E-mail')
+            var randomEmail = faker.internet.email();
+            console.log(randomEmail)
+            arrayCad.push(randomEmail)
+
         }else if (opcao == 'Endereco'){
-            console.log('Endereco')
+            var address = faker.address.streetName()
+            console.log('Endereco:' + address)
+            arrayCad.push(address)
+
         }else if (opcao == 'Cidade'){
-            console.log('Cidade')
+            var cidade = faker.address.city()
+            console.log('Cidade:' + cidade)
+            arrayCad.push(cidade)
+
         }else if (opcao == 'Pais'){
-            console.log('Pais')
+            var pais = faker.address.country()
+            console.log('Cidade:' + pais)
+            arrayCad.push(pais)
+
         }else if (opcao == 'Telefone'){
-            console.log('Telefone')
+            var telefone = faker.phone.phoneNumber()
+            console.log('Telefone: ' + telefone)
+            arrayCad.push(telefone)
+
+        }else if (opcao == 'Funcao'){
+            var Funcao = faker.name.jobType()
+            console.log('Funcao: ' + Funcao)
+            arrayCad.push(Funcao)
+
+        }else if (opcao == 'JobTitle'){
+            var JobTitle = faker.name.jobTitle()
+            console.log('JobTitle: ' + JobTitle)
+            arrayCad.push(JobTitle)
+
+        }else if (opcao == 'jobArea'){
+            var jobArea = faker.name.jobArea()
+            console.log('jobArea: ' + jobArea)
+            arrayCad.push(jobArea)
+
+        }else if (opcao == 'CEP'){
+            var CEP = faker.address.zipCode()
+            console.log('CEP: ' + CEP)
+            arrayCad.push(CEP)
+
+        }else if (opcao == 'salario'){
+            var salario = faker.finance.amount()
+            console.log('salario: ' + salario)
+            arrayCad.push(salario)
         }
     }
+    console.log(arrayCad)
+
+}
+
+//console.log(faker.fake("{{name.lastName}}, {{name.firstName}} {{name.suffix}}"));
+
+function cadBanco(){
+
+
+
+
 }
